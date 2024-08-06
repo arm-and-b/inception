af55b915dc46225710bc09b20201d984e6dff8fc
arm-and-b <binetarmand@free.fr>
2024-08-06 21:02:03 +0200

    fix bugs, problem sur les logs

--- inception/srcs/requirements/mariadb/tools/mysql.sh
+++ inception/srcs/requirements/mariadb/tools/mysql.sh
@@ -2,19 +2,25 @@
 
 service mariadb start
 
-sleep 10
+sleep 5
 
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 
 #mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'wordpress.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'wordpress.srcs_inception' WITH GRANT OPTION;"
 #mysql -uroot -e "ALTER USER '${MYSQL_USER}'@'wordpress.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;"
-mysql -uroot -e "ALTER USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
 
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "SHUTDOWN;"
+#mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+#mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "SHUTDOWN;"
 
-mysqld --user=root
+sleep 1
+
+mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
--- inception/srcs/requirements/wordpress/tools/wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress.sh
@@ -2,29 +2,24 @@
 
 sleep 1
 
-if [ ! -e /var/www/wordpress/ ]
+if [ -f /var/www/wordpress/wp-config.php ];
 then
+	echo "WP INSTALLED";
+else
+	wp core download --allow-root
+	
+  	wp config create --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost=mariadb:3306 --allow-root
+  	sleep 5
 
-	wp core download --allow-root --path='/var/www/wordpress'
+  	wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
+  	sleep 5
 
+  	wp user create --role=author "${WP_USER_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --allow-root >> /log.txt
 fi
 
-if [ ! -e /var/www/wordpress/wp-config.php ]
-then
-  # https://github.com/wp-cli/wp-cli/issues/5933 (Could not create 'wp-config.php' with wp-cli config create #5933)
-  cd /var/www/wordpress || return
+wp plugin list --allow-root
 
-  wp config create --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost=mariadb:3306 --allow-root
-  sleep 5
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
 
-  wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
-  sleep 5
-
-  wp user create --role=author "${WP_USER_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --allow-root >> /log.txt
-fi
-
-service php7.4-fpm start
-
-service php7.4-fpm stop
-
-php-fpm7.4 -F 
+exec /usr/sbin/php-fpm7.4 -F
